e05c5d09ae1c7abaa3232cb801bdf0bfca07f755
lakonguina <laury.akonguina@icloud.com>
2026-01-13 11:56:52 +0100

    ghostty + nvim

---
+++ init.lua
@@ -0,0 +1,89 @@
+vim.api.nvim_create_autocmd("ColorScheme", {
+	pattern = "*",
+	callback = function()
+		-- Récupère tous les groupes de highlight
+		local highlights = vim.fn.getcompletion("", "highlight")
+
+		for _, hl in ipairs(highlights) do
+			-- Garde uniquement les groupes liés aux commentaires
+			if not hl:match("Comment") and not hl:match("comment") then
+				vim.api.nvim_set_hl(0, hl, { link = "Normal" })
+			end
+		end
+
+		-- Définit la couleur des commentaires
+		vim.api.nvim_set_hl(0, "Comment", { fg = "#5faf5f", italic = true })
+	end,
+})
+
+-- Déclenche après le chargement du colorscheme
+vim.cmd("colorscheme default")
+-- Options
+vim.opt.cursorline = true
+vim.opt.number = true
+vim.opt.title = true
+
+-- Tabs size
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+	spec = {
+		-- add your plugins here
+		{
+			"mason-org/mason.nvim",
+			opts = {},
+		},
+		{
+			"stevearc/conform.nvim",
+			opts = {
+				formatters_by_ft = {
+					lua = { "stylua" },
+					python = { "isort", "black" },
+					rust = { "rustfmt", lsp_format = "fallback" },
+					javascript = { "prettierd", "prettier", stop_after_first = true },
+					javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+					typescript = { "prettierd", "prettier", stop_after_first = true },
+					typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+				},
+			},
+		},
+	},
+	performance = { rtp = { reset = false } },
+	-- colorscheme that will be used when installing plugins.
+	-- install = { colorscheme = { "habamax" } },
+	-- automatically check for plugin updates
+	-- checker = { enabled = true },
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
+})
